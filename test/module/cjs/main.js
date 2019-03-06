4ca944f825983f903e5b412738e35a6142b90eb8
higuri <higuri36@gmail.com>
2019-03-07 00:52:52 +0900

    Add test/module/cjs

---
+++ test/module/cjs/main.js
@@ -0,0 +1,14 @@
+// cjs/main.js
+// TODO: ts
+
+// relative to 'index.html'
+const DrawBetween = require('../../../dist/drawbetween_cjs.js');
+
+// main()
+function main() {
+  const canvas = document.querySelector('#canvas');
+  const draw = new DrawBetween(canvas);
+  draw.rects({x: 1, y: 1}, {x: 279, y: 279});
+}
+
+document.addEventListener('DOMContentLoaded', () => main());
--- test/module/cjs/run.js
+++ test/module/cjs/run.js
@@ -1,3 +1,10 @@
 // cjs/run.js
+const browserify = require('browserify');
+const fs = require('fs');
 const opn = require('opn');
+
+browserify()
+  .add(__dirname + '/main.js')
+  .bundle()
+  .pipe(fs.createWriteStream(__dirname + '/bundle.js'));
 opn(__dirname + '/index.html');

bb24d04eed12b135e5d6a0427566e25df9073664
higuri <higuri36@gmail.com>
2019-03-11 00:41:02 +0900

    Fix setting up test

--- node_scripts/test.js
+++ node_scripts/test.js
@@ -3,24 +3,26 @@
 const {execSync} = require('./utils');
 const {pushd, popd} = require('./utils');
 
-execSync('yarn clean');
-execSync('yarn build:modules');
-execSync('yarn build:pack');
+execSync('npm run clean');
+execSync('npm run build:modules');
+execSync('npm run build:pack');
 
 // test/module/cjs/
 pushd('./test/module/cjs/');
-execSync('yarn install');
-execSync('yarn build');
-execSync('yarn start');
+// CAUTION: Don't use `yarn install` here. Use `npm install`.
+// https://github.com/yarnpkg/yarn/issues/2165
+execSync('npm install');
+execSync('npm run build');
+execSync('npm run start');
 popd();
 // test/module/esm/
 pushd('./test/module/esm/');
-execSync('yarn install');
-execSync('yarn build');
-execSync('yarn start');
+execSync('npm install');
+execSync('npm run build');
+execSync('npm run start');
 popd();
 // test/module/iife/
 pushd('./test/module/iife/');
-execSync('yarn install');
-execSync('yarn build');
-execSync('yarn start');
+execSync('npm install');
+execSync('npm run build');
+execSync('npm run start');

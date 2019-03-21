ac7954b4a2264d1ee27cd53745ccba1e5b0d4915
higuri <higuri36@gmail.com>
2019-03-21 23:22:30 +0900

    Cleanup env

---
+++ npm_scripts/sample.js
@@ -0,0 +1,12 @@
+// sample.js
+// TODO:
+// * watch & hot reloading
+
+const {execSync} = require('./utils');
+
+// build:src
+execSync('npm run build');
+// build:sample
+execSync('npx browserify ./sample/main.js -o ./sample/bundle.js');
+// start:sample
+execSync('npx opn ./sample/index.html');
